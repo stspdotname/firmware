28befa32a69e6158055a4e316c13acf03a209fee
Stefan Sperling <stsp@stsp.name>
2015-01-18 19:56:45 +0100

    wrt54gl support

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # default parameters for Makefile
 SHELL:=$(shell which bash)
-TARGET=ar71xx
-PACKAGES_LIST_DEFAULT=vpn
+TARGET=brcm47xx
+PACKAGES_LIST_DEFAULT=wrt54g
 OPENWRT_SRC=git://git.openwrt.org/14.07/openwrt.git
 OPENWRT_COMMIT=331ecb083c3e7409bfa9c4d6d2e3db06dede3dfd
 MAKE_ARGS=
