ca410e3268a609ba13dae4232dda89937ab94fff
Stefan Sperling <stsp@stsp.name>
2015-05-08 23:54:40 +0200

    trimmed-down nanostation firmware

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # default parameters for Makefile
 SHELL:=$(shell which bash)
 TARGET=ar71xx
-PACKAGES_LIST_DEFAULT=default backbone
+PACKAGES_LIST_DEFAULT=nanostation
 OPENWRT_SRC=git://git.openwrt.org/14.07/openwrt.git
 OPENWRT_COMMIT=229d60fdb45c34902d402938e231c006f7c73931
 MAKE_ARGS=
